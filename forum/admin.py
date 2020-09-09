d85588733db2613f0332b51f391dbc723e717d5a
liuzhenxin2 <liuzhenxin1@live.com>
2020-09-09 06:39:05 +0000

    registered models

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ForumTopic, ForumComments
 # Register your models here.
+
+admin.site.register(ForumTopic)
+admin.site.register(ForumComments)
--- lessons/admin.py
+++ lessons/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Lessons
+from .models import Lessons, Subtopics
 # Register your models here.
 
 admin.site.register(Lessons)
+admin.site.register(Subtopics)
