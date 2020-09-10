c65fc9c8cc33913a5f196878623cda6d971ad80c
liuzhenxin2 <liuzhenxin1@live.com>
2020-09-10 08:13:25 +0000

    added test for home route

--- RuleOf72Project/urls.py
+++ RuleOf72Project/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('lessons/', include('lessons.urls')),
+    path('', include('home.urls')),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'home.template.html')
