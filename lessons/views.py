662c19d0ad92a32bc0eadfaa1d815b9703f37897
liuzhenxin2 <liuzhenxin1@live.com>
2020-09-28 16:57:51 +0000

    edit and delete buttons only show for creator

--- lessons/views.py
+++ lessons/views.py
@@ -51,26 +51,29 @@ def create_lesson(request):
 def update_lesson(request, lesson_id):
     # 1. retrieve the book that we are editing
     lesson_being_updated = get_object_or_404(Lesson, pk=lesson_id)
-
+    if request.user == lesson_being_updated.teacher:
     # 2. if the update form is submitted
-    if request.method == "POST":
+        if request.method == "POST":
 
-        # 3. create the form and fill in the user's data. Also specify that
-        # this is to update an existing model (the instance argument)
-        lesson_form = Lessons_form(request.POST, instance=lesson_being_updated)
-        if lesson_form.is_valid():
-            lesson_form.save()
-            return redirect(reverse(all_lessons))
+            # 3. create the form and fill in the user's data. Also specify that
+            # this is to update an existing model (the instance argument)
+            lesson_form = Lessons_form(request.POST, instance=lesson_being_updated)
+            if lesson_form.is_valid():
+                lesson_form.save()
+                return redirect(reverse(all_lessons))
+            else:
+                return render(request, 'update_lessons.template.html', {
+                    "form": lesson_form
+                })
         else:
+            # 4. create a form with the book details filled in
+            lesson_form = Lessons_form(instance=lesson_being_updated)
             return render(request, 'update_lessons.template.html', {
                 "form": lesson_form
             })
+    
     else:
-        # 4. create a form with the book details filled in
-        lesson_form = Lessons_form(instance=lesson_being_updated)
-        return render(request, 'update_lessons.template.html', {
-            "form": lesson_form
-        })
+        return redirect(reverse(all_lessons))
 
 @login_required    
 def delete_lesson(request, lesson_id):
