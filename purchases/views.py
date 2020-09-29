8e0ff298580d391cb1f30bb4758f5b754c8b1c7f
liuzhenxin2 <liuzhenxin1@live.com>
2020-09-29 11:16:34 +0000

    added page for bought lessons

--- purchases/views.py
+++ purchases/views.py
@@ -5,7 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 import json
 import stripe
-from lessons.models import Lesson
+from lessons.models import Lesson, Sub_topic
+from forum.models import Forum_topic, Forum_comment
 from .models import Purchase
 
 def checkout(request, lesson_id):
@@ -85,110 +86,3 @@ def handle_payment(session):
     purchase.save()
 
 
-# from django.shortcuts import render, get_object_or_404, HttpResponse, reverse
-# from django.conf import settings
-# from django.contrib.sites.models import Site
-# from django.views.decorators.csrf import csrf_exempt
-# from django.contrib.auth.models import User
-# import stripe
-# import json
-
-# # Create your views here.
-
-
-# def checkout(request):
-
-#     # setup stripe
-#     stripe.api_key = settings.STRIPE_SECRET_KEY
-
-#     # retrive the shopping cart
-#     # store all the line items in this list
-#     line_items = []
-
-#     # for each item in the shopping cart...
-#     item = {
-#         "name": 'aa',
-#         # multiply the actual cost of the book to represent
-#         # the cost in cents and must be in integer
-#         "amount": 1000,
-#         "quantity": 1,
-#         "currency": "usd"
-#     }
-
-#     line_items.append(item)
-#     # get the domain name
-#     current_site = Site.objects.get_current()
-#     domain = current_site.domain
-
-#     # pass all the line items to stripe and in return
-#     # get a checkout session id
-#     session = stripe.checkout.Session.create(
-#         payment_method_types=['card'],
-#         line_items=line_items,
-#         client_reference_id=request.user.id,
-#         mode='payment',
-#         success_url=domain + reverse('checkout_success_route'),
-#         cancel_url=domain + reverse('checkout_cancelled_route'),
-#         metadata={
-#             "books": json.dumps([1])
-#         }
-#     )
-
-#     # render the template which will redirect to stripe
-#     return render(request, 'purchases/checkout.template.html', {
-#         'session_id': session.id,
-#         'public_key': settings.STRIPE_PUBLISHABLE_KEY
-#     })
-
-
-# def checkout_success(request):
-#     # empty the shopping cart
-#     # request.session['shopping_cart'] = {}
-#     return HttpResponse("Payment is successful")
-
-
-# def checkout_cancelled(request):
-#     return HttpResponse("Payment cancelled")
-
-
-# @csrf_exempt
-# def payment_completed(request):
-#     # payload represents the data sent back to us by Stripe
-#     payload = request.body
-#     endpoint_secret = settings.STRIPE_ENDPOINT_SECRET
-#     sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
-#     event = None
-
-#     try:
-#         event = stripe.Webhook.construct_event(
-#             payload, sig_header, endpoint_secret
-#         )
-#     except ValueError as e:
-#         # invalid payload
-#         print(e)
-#         return HttpResponse(status=400)
-#     except stripe.error.SignatureVerificationError as e:
-#         print(e)
-#         return HttpResponse(status=400)
-
-#     if event['type'] == 'checkout.session.completed':
-#         session = event['data']['object']
-#         # handle_payment(session)
-
-#     return HttpResponse(status=200)
-
-
-# def handle_payment(session):
-#     user = get_object_or_404(User, pk=session["client_reference_id"])
-
-#     all_book_ids = json.loads(session['metadata']['books'])
-#     for book in all_book_ids:
-#         book_model = get_object_or_404(Book, pk=book["book_id"])
-
-#         # create a Purchase model manually
-#         purchase = Purchase()
-#         purchase.book = book_model
-#         purchase.user = user
-#         purchase.price = book_model.cost
-#         purchase.qty = int(book["qty"])
-#         purchase.save()
