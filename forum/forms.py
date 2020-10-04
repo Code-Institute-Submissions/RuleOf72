37c6cf1fb7982f147e10482a70e62c2f07eecaa9
liuzhenxin2 <liuzhenxin1@live.com>
2020-10-04 07:07:47 +0000

    made forum code PEP8 compliant

--- forum/forms.py
+++ forum/forms.py
@@ -1,17 +1,20 @@
 from django import forms
 from lessons.models import Lesson, Sub_topic
-from .models import Forum_comment, Forum_topic 
+from .models import Forum_comment, Forum_topic
 from django.contrib.auth.models import User
+
 
 class Forum_form(forms.ModelForm):
     class Meta:
         model = Forum_topic
         fields = ('title', 'content')
 
+
 class Comment_form(forms.ModelForm):
     class Meta:
         model = Forum_comment
         fields = ('title', 'content')
+
 
 class SearchForm(forms.Form):
     title = forms.CharField(max_length=255, required=False)
