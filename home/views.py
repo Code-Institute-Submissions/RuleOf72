4ecb5d273b3ee078d88bf08cff86061ccdb4c473
liuzhenxin2 <liuzhenxin1@live.com>
2020-10-04 07:08:56 +0000

    made home app PEP8 compliant

--- home/views.py
+++ home/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
 
-# Create your views here.
 
 @login_required
 def home(request):
     return render(request, 'home/home.template.html')
-
-
